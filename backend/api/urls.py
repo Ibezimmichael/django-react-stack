bcd149ebb66556630eb18a17916abf349aaa4bea
Ibezimmichael <ibezim.michael@yahoo.com>
2024-06-28 08:19:36 +0100

    Map urls in api to project level url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="notes"),
-    path("notes/<int:pk>", views.NoteListCreate.as_view(), name="notes"),
+    path("notes/delete/<int:pk>", views.NoteDelete.as_view(), name="notes"),
 ]
